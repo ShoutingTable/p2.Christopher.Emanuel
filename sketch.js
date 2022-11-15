4d5c3b8c05d90013d4e3e595a5fec2122f4d60c0
ShoutingTable <Christopheremanuel97@gmail.com>
2022-11-15 01:22:47 -0600

    health icons remaining

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ function setup() {
     // brightness slider
     colorMode(HSB);
     slider = createSlider(0, 360, 60, 40)
-    slider.position(600, 900)
+    slider.position(600, 930)
     slider.style('width', '800px')
 
     // webcam capture
@@ -47,35 +47,55 @@ function draw() {
     background(val, 100, 100, 1);
 
     // webcam value
-    image(capture, 600, 40, 800, 800);
+    image(capture, 600, 110, 800, 800);
 
     // digital clock
     clock();
 
     // weather
     textSize(30);
-    text("City: Lubbock", 110, 50);
-    text("Current temperature: " + temperature, 110, 70);
-    text("Forecast: " + weather, 110, 90);
+    text("City: Lubbock", 30, 850);
+    text("Current temperature: " + temperature, 30, 880);
+    text("Forecast: " + weather, 30, 910);
 
     // news
-    text("The news today:" + ntemp, 250, 200, 100, 100);
+    textSize(25);
+    text("The News Today:", 1500, 100, 400, 300);
+    text("_________________", 1500, 105, 400, 300);
+    textSize(25);
+    text("•", 1500, 160)
+    textSize(15);
+    text(ntemp, 1510, 145, 200);
+    textSize(25);
+    text("•", 1500, 342)
+    textSize(15);
+    text(ntemp2, 1510, 325, 200);
 
     // calendar
     let m = month();
-    text('Current month: \n' + m, 50, 550);
+    textSize(60);
+    text(m + '/', 1220, 100);
     let d = day()
-    text('Current day: \n' + d, 50, 650);
-    text("To Do List", 250, 650);
+    text(d, 1300, 100);
+    textSize(50);
+    text("To Do List", 30, 150);
+    text("11/15", 390, 150);
+    text("_________________", 40, 150);
+    text("• Go to class - 11 AM", 35, 230);
+    text("• Go to the gym - 1 PM", 35, 303);
+    text("• Get groceries - 3 PM", 35, 380);
+    text("• Cook Dinner - 5 PM", 35, 460);
+    text("• Do homework - 7 PM", 35, 540);
 
 
 }
+
 
 // clock function
 function clock() {
     fill("black");
     // textFont(clockFont);
-    textAlign(CENTER, CENTER);
+    // textAlign(CENTER, TOP);
     textSize(100);
     let Hour = hour();
     let min = minute();
@@ -84,10 +104,11 @@ function clock() {
     if (min < 10)
         min = "0" + min
     Hour %= 12
-    text(Hour + ":" + min + ":" + secs + noon, width / 2, height / 2);
+    text(Hour + ":" + min + ":" + secs + noon, 620, 100);
 }
 
 function globalNews() {
     ntemp = json1.articles[0].description;
+    ntemp2 = json1.articles[1].description;
 
 }
